22de8e18b4b660b3ba0b2e91622b1dad492809f9
Yipeng Sun <syp@umd.edu>
2023-09-15 14:15:46 +0800

    Add a 'vi' alias.

--- posh/Microsoft.PowerShell_profile.ps1
+++ posh/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,13 @@
-oh-my-posh init pwsh | Invoke-Expression
-
 # line editor
 Set-PSReadLineOption -EditMode Emacs
 
 # aliases
 # NOTE: do not use New-Alias as that prodcues an error on profile reload!
 Set-Alias -Name g -Value git
+Set-Alias -Name vi -Value vim
 
 # 'ls' already exist. just remove it
 # PowerShell 5
 Remove-Item Alias:ls
+
+oh-my-posh init pwsh | Invoke-Expression
