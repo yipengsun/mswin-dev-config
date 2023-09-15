8deb3e07c18468080d0c6cda5fc3299c0432f723
Yipeng Sun <syp@umd.edu>
2023-09-15 13:59:07 +0800

    Make GNU coreutils available.

--- setpath.ps1
+++ setpath.ps1
@@ -28,5 +28,8 @@ param(
 $cmdpath = -join([Environment]::GetEnvironmentVariable("USERPROFILE"), "\", "commands")
 AddTo-Path $cmdpath
 
+# for GNU coreutils on Windows
+AddTo-Path "C:\Program Files\Git\usr\bin"
+
 # print out
 Get-Path User
