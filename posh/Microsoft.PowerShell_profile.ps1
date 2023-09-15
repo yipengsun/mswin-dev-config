60b637c1d2490aee751c76dff445baba908a304a
Yipeng Sun <syp@umd.edu>
2023-09-15 14:12:23 +0800

    Configure posh readline mode.

--- posh/Microsoft.PowerShell_profile.ps1
+++ posh/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,8 @@
 oh-my-posh init pwsh | Invoke-Expression
 
+# line editor
+Set-PSReadLineOption -EditMode Emacs
+
 # aliases
 # NOTE: do not use New-Alias as that prodcues an error on profile reload!
 Set-Alias -Name g -Value git
