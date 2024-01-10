4d34132bb535cb2ebbbedddaee22dd2e49da4db1
Yipeng Sun <syp@umd.edu>
2024-01-10 11:12:45 +0800

    Also set Java CLASSPATH.

--- setpath.ps1
+++ setpath.ps1
@@ -1,30 +1,45 @@
 #!/usr/bin/env powershell.exe
 
+function Get-EnvVar{
+param(
+    [string]$User,
+    [string]$Var
+)
+    $value = [Environment]::GetEnvironmentVariable($Var, $User)
+    Write-output $value
+}
+
+function AddTo-EnvVar{
+param(
+    [string]$Dir,
+    [string]$Var
+)
+    if( !(Test-Path $Dir) ){
+        Write-warning "Supplied directory was not found!"
+        return
+    }
+
+    $value = [Environment]::GetEnvironmentVariable($Var, "User")
+    if( $value -notlike "*"+$Dir+"*" ){
+        [Environment]::SetEnvironmentVariable($Var, "$Dir;$value", "User")
+    }
+}
+
 function Get-Path{
 param(
     [string]$User
 )
-
-    $path = [Environment]::GetEnvironmentVariable("PATH", $User)
-    Write-output $path
+    Get-EnvVar $User "PATH"
 }
 
 function AddTo-Path{
 param(
     [string]$Dir
 )
-
-    if( !(Test-Path $Dir) ){
-        Write-warning "Supplied directory was not found!"
-        return
-    }
-    $PATH = [Environment]::GetEnvironmentVariable("PATH", "User")
-    if( $PATH -notlike "*"+$Dir+"*" ){
-        [Environment]::SetEnvironmentVariable("PATH", "$Dir;$PATH", "User")
-    }
+    AddTo-EnvVar $Dir "PATH"
 }
 
-# set path
+# set PATH
 $cmdpath = -join([Environment]::GetEnvironmentVariable("USERPROFILE"), "\", "commands")
 AddTo-Path $cmdpath
 
@@ -37,5 +52,9 @@ AddTo-Path "C:\Program Files\LLVM\bin"
 # for make
 #AddTo-Path "C:\Program Files (x86)\GnuWin32\bin"
 
+# set java CLASSPATH
+AddTo-EnvVar $cmdpath CLASSPATH
+
 # print out
 Get-Path User
+Get-EnvVar User CLASSPATH
