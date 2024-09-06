7282fd0c7d9506e2c84be07d5672583a8c09e727
Yipeng Sun <syp@umd.edu>
2024-09-06 16:38:35 +0800

    Fix: use the correct folder.

--- tools/config-nvim.ps1
+++ tools/config-nvim.ps1
@@ -1,25 +1,15 @@
 $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $parentDir = Split-Path -Path $scriptDir -Parent
 
-$configDir = -join($env:USERPROFILE, '\.config')
-if (!(Test-Path -path $configDir)) {
-    New-Item $configDir -Type Directory
-}
-
-$nvimDir = -join($configDir, '\nvim')
-if (!(Test-Path -path $nvimDir)) {
-    New-Item $nvimDir -Type Directory
-}
-
-$spaceVimDir = -join($nvimDir, '\SpaceVim')
+$nvimDir = -join($env:LOCALAPPDATA, '\nvim')
 $spaceVimConfigDir = -join($env:USERPROFILE, '\.SpaceVim.d')
 
 
 # link SpaceVim bundle
 $localSpaceVimDir = -join($parentDir, '\submodules\SpaceVim')
-if (!(Test-Path -path $spaceVimDir)) {
+if (!(Test-Path -path $nvimDir)) {
     Write-Host "Link SpaceVim dir..." -ForegroundColor Green
-    New-Item -ItemType SymbolicLink -Path $spaceVimDir -Target $localSpaceVimDir
+    New-Item -ItemType SymbolicLink -Path $nvimDir -Target $localSpaceVimDir
 }
 
 # link SpaceVim config
